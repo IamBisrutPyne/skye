fd1f80fe1f1f8e9311f133aa4151340481bcf142
Bisrut Pyne <bisrutpyne@gmail.com>
2021-10-22 13:07:38 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Footer = () => (
+    <div className="text-center font-semibold font-sans text-xl p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+    <a href="https://iambisrutpyne.github.io" target="_blank">© Bisrut Pyne &nbsp;</a>
+    <a href="https://github.com/IamBisrutPyne" target="_blank">GitHub</a>
+  </div>
+);
